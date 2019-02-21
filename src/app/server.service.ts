d57bf3ae7c8246cb04b7133d54f0c67f14c1269e
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-02-21 15:12:47 -0400

    Added Get Request

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -8,4 +8,9 @@ export class ServerService {
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('https://udemy-ng-http-5c9e5.firebaseio.com/data.json', servers, {headers: headers});
     }
+
+    getServers() {
+        return this.http.get('https://udemy-ng-http-5c9e5.firebaseio.com/data.json');
+    }
+
 }
